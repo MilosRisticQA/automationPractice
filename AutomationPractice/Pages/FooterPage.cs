1a3f21307b4cd77a46f532e4265541814e833faa
TheNixa <milosevic.edu@gmail.com>
2018-12-30 12:45:19 +0100

    Footer test finished in class

---
+++ AutomationPractice/Pages/FooterPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPractice.Pages
+{   
+    class FooterPage
+    {
+        readonly IWebDriver driver;
+
+        public FooterPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.ClassName("footer-container")));
+        }
+
+        public void ClickOnLink(string link)
+        {
+            By clickOnLink = By.CssSelector(".bullet [title='" + link + "']");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(clickOnLink)).Click();
+        }
+
+        public bool PageIsDisplayed(string page)
+        {
+            By elementIsDisplayed = By.XPath("//h1[contains(text(),'"+page+"')]");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(elementIsDisplayed)).Displayed;
+        }
+
+    }
+}
---
+++ AutomationPractice/Steps/FooterSteps.cs
@@ -0,0 +1,32 @@
+﻿using AutomationPractice.Helpers;
+using AutomationPractice.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace AutomationPractice.Steps
+{
+    [Binding]
+    public class FooterSteps : Base
+    {
+        FooterPage fp = new FooterPage(Driver);
+
+        [When(@"user clicks on '(.*)' option")]
+        public void WhenUserClicksOnOption(string myAccLink)
+        {
+            fp.ClickOnLink(myAccLink);
+        }
+
+        [Then(@"correct '(.*)' is displayed")]
+        public void ThenCorrectIsDisplayed(string page)
+        {
+            Assert.True(fp.PageIsDisplayed(page), "Page is not displayed");
+        }
+
+
+    }
+}
